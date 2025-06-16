deea4fdccb14839e6f4c690576a47fa4688d739a
Yanis0003 <yaniiis12@hotmail.fr>
2025-06-16 16:29:21 +0200

    premier scipt Javascript

---
+++ Script/test.js
@@ -0,0 +1,37 @@
+let targetNumber = 12;
+
+let userNumber = prompt('devinez un nombre entre 1 et 100 :');
+
+while(userNumber != targetNumber) {
+    
+    if ( userNumber > targetNumber){
+        alert('raté trop grand');
+    } else {
+        alert ("c'est trop petit");
+    }
+
+    userNumber = prompt('devinez un nombre entre 1 et 100');
+}
+
+alert('gg wp');
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+let firstname = prompt('Quel est votre prénom ?');
+let lastname = prompt('Quel est votre Nom ?');
+
+alert(`Bonjour ${firstname} ${lastname} ! `);
+*/
